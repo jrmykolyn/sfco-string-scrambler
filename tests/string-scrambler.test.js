47a607c4862763888bbf693da3629fd40710b897
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-07 18:16:04 -0400

    Stubbed out initial tests re: updating module to support an 'options' argument.

--- tests/string-scrambler.test.js
+++ tests/string-scrambler.test.js
@@ -32,3 +32,13 @@ test( 'When invoked with a valid string, function returns a "scrambled" string o
 
 	t.is( input.length, output.length );
 } );
+
+test.todo( 'When invoked with an `options` argument, function does not throw an error' );
+
+test.todo( 'When invoked with an `options` argument containing a valid `restrictTo` property, function restricts the output to the correct characters.' );
+
+test.todo( 'When invoked with an `options` argument containing an invalid `restrictTo` property, function falls back to the default, restriction-free behaviour.' );
+
+test.todo( 'When invoked with an `options` argument containing a valid `useChars` property, function restricts the output to the characters provided.' );
+
+test.todo( 'When invoked with an `options` argument containing an invalid `useChars` property, function falls back to the default behaviour.' );

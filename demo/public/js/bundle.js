f18bdc673b4cbe182736958143af54181264104e
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-07 17:55:40 -0400

    gh-pages | Completed structure, styles, and logic for initial demo.

--- demo/public/js/bundle.js
+++ demo/public/js/bundle.js
@@ -87,15 +87,64 @@ const CHARS = [
 module.exports = CHARS;
 
 },{}],4:[function(require,module,exports){
-// DECLARE VARS
-var stringScrambler = require( '../../../index' );
-var el = document.getElementById( 'msg' );
-var msgText = el.innerHTML;
+( function() {
+	// --------------------------------------------------
+	// DECLARE VARS
+	// --------------------------------------------------
+	var stringScrambler = require( '../../../index' );
+	var btns = document.querySelectorAll( 'button' );
 
-// INIT
-setInterval( function() {
-	el.innerHTML = stringScrambler( msgText );
-}, 1500 );
+	// --------------------------------------------------
+	// REGISTER EVENT LISTENERS
+	// --------------------------------------------------
+	btns.forEach( function( button ) {
+		button.addEventListener( 'click', function() {
+			var parent = this.parentElement;
+			var target = parent.querySelector( '.target' );
+			var text = target.innerHTML;
+			var opts = getOptions( parent );
+
+			/// TODO[@jrmykolyn] - Pass `opts` into `stringScrambler` call... when possible.
+
+			target.innerHTML = stringScrambler( text );
+		} );
+	} );
+
+	// --------------------------------------------------
+	// DECLARE FUNCTIONS
+	// --------------------------------------------------
+	function getOptions( node ) {
+		if ( !node || !node.getAttribute( 'data-options' ) ) {
+			return null;
+		} else {
+			return node.getAttribute( 'data-options' )
+				.split( ';' )
+				.map( function( opt ) {
+					return opt.replace( / /gmi, '' );
+				} )
+				.map( function( opt ) {
+					return opt.split( ':' );
+				} )
+				.reduce( function( obj, opt ) {
+					var key = opt[ 0 ];
+					var value = opt[ 1 ];
+
+					// Parse value
+					if ( value.toString().toLowerCase() === 'true' || value.toString().toLowerCase() === 'false' ) {
+						value = ( value === 'true' );
+					}
+
+					if ( !isNaN( +value ) ) {
+						value = +value;
+					}
+
+					obj[ key ] = value;
+
+					return obj;
+				}, {} );
+		}
+	}
+} )();
 
 },{"../../../index":5}],5:[function(require,module,exports){
 // --------------------------------------------------
